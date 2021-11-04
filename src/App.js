de72fbe455de9d94aae9aaa8e2f9361c3d47a234
Soumya Mukherjee <rivu10@gmail.com>
2021-11-04 09:28:17 +0530

    Implementation of Class Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './component/Functional_Component_Example';
+import Welcome from './component/Class_Component_Example';
 
 function App() {
   return (
     <div className="App">
-      <Greet/>
+      {/* <Greet/> */}
+      <Welcome/>
     </div>
   );
 }
