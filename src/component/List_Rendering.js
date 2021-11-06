574064bb33a20f487ce897103ea908a2b2ba3f4f
Soumya Mukherjee <rivu10@gmail.com>
2021-11-06 10:53:48 +0530

    Person list rendering from same component

--- src/component/List_Rendering.js
+++ src/component/List_Rendering.js
@@ -10,12 +10,18 @@ function List_Rendering() {
         </div>
     ) */
 
-    const names = ['Soumya', 'Avik', 'Dayan']
+    /*const names = ['Soumya', 'Avik', 'Dayan']
     const nameList = names.map(name => <div><h4>{name}</h4></div>)
     return (
         <div>
             {nameList}
         </div>
+    )*/
+
+    const persons = [{"id": 1, "name": "Soumya", "age": 10}, {"id": 1, "name": "Avik", "age": 20}, {"id": 2, "name": "Priyanka", "age": 30}]
+    const personList = persons.map(person => (<div> <h4>I'm {person.name}. I am {person.age} years old.</h4></div>))
+    return (
+        <div>{personList}</div>
     )
 }
 
