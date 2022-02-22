e6d4fef9d05105fe13e1266079383acd586cd515
Soumya Mukherjee <soumukhe2@publicisgroupe.net>
2022-02-22 09:43:27 +0530

    React Mounting Functions notes

--- src/component/LifeCycleA.js
+++ src/component/LifeCycleA.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react'
 import LifeCycleA_Child from './LifeCycleA_Child';
 
 class LifeCycleA extends Component {
+
+    /*
+    Mounting Functions :
+    ---------------------------------------------------------
+    Function Name               Order
+    constructor(props)           1
+    getDerivedStateFromProps(props, state)      2
+    render                      3
+    componentDidMount()         4
+    */
+
+
+    /*
+    A special function that will be called whenever a new component is created.
+    Use: 1. Initialize state
+         2. Binding the event handlers.
+    
+    Precaustion: Never use any side effects like HTTP Requests
+
+    super(props) --- first statement
+    directly overwrite by this.state within constructor. In Other place, we should use setState().
+    */
     constructor(props) {
         super(props)
     
@@ -11,11 +33,23 @@ class LifeCycleA extends Component {
         console.log('LifeCycleA constructor called')
     }
 
+    /*
+    When the state of the component depends on the changes in props over the time.
+    As it's a static method, this method can't have this keyword. So this.setState() method can't be called.
+    Instead you need to return an object that represents new state of the component.
+    
+    Precaustion: Never use any side effects like HTTP Requests
+    */
     static getDerivedStateFromProps(props, state) {
         console.log(`LifeCycleA getDerivedStateFromProps called`);
         return null;
     }
 
+    /*
+     Invoked immediately after a component and all its children components have been rendered to the DOM.
+     Use: Cause side effects e.g. Interact with the DOM or perform any ajax calls to load data.
+
+    */
     componentDidMount(){
         console.log(`LifeCycleA componentDidMount called`);
     }
@@ -40,7 +74,13 @@ class LifeCycleA extends Component {
         });
     }
     
-    
+    /*
+    Only required method in a class component.
+    In this method, we simply read props and state and return JSX.
+    It's a Pure function.
+    Precaution: Don't change any state or interact with DOM or make any ajax calls.
+    As it contains the children components, children components lifecycle methods are also executed.
+    */
     render() {
         console.log(`LifeCycleA render method is called`);
         return (
