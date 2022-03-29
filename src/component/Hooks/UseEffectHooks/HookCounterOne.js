066cc090a57c64ee6e6fd726062f7450d06a05db
Soumya Mukherjee <rivu10@gmail.com>
2022-03-29 20:54:50 +0530

    Conditional useEffect run

--- src/component/Hooks/UseEffectHooks/HookCounterOne.js
+++ src/component/Hooks/UseEffectHooks/HookCounterOne.js
@@ -2,15 +2,19 @@ import React, {useState, useEffect} from 'react'
 
 function HookCounterOne() {
     const [count, setCount] = useState(0);
+    const [name, setName] = useState('');
     /**
      * When we are defining useEffect,  basically we are requesting react to execute this 
      * function when component renders i.e. ComponentDidMount & ComponentDidUpdate
      */
     useEffect(()=> {
+        console.log(`useEffect - conditional run`);
         document.title = `Clicked ${count} times`;
-    })
+    }, [count]);
+    
   return (
     <>
+        <input type="text" name="name" value={name} onChange={e=> setName(e.target.value)}></input>
         <button onClick={()=> setCount(prevCount => prevCount+1)}>Click {count} times</button>
     </>
   )
